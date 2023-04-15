e51b2afac911b3e601d925662ecfd8c10cb2a712
pokabilius <georgemirsilidis@hotmail.com>
2023-04-16 01:14:42 +0300

    Make books appear after submit

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-let myLibrary = [];
 
+let myLibrary = [];
+let newBook = {}
+let totalPages = 0
+let totalBooks = 0
+let booksInProgress = 0
+
+const submitbtn = document.getElementById('submit')
 const btn = document.querySelector('button')
 const input = document.querySelector('.formcontainer')
-const modal = document.querySelector('.modal')
-const divTitle = document.createElement('div')
-const divAuthor = document.createElement('div')
-const divPages = document.createElement('div')
-const divRead = document.createElement('div')
-modal.appendChild(divTitle)
-modal.appendChild(divAuthor)
-modal.appendChild(divPages)
-modal.appendChild(divRead)
+const totalBooksText = document.querySelector('.finished')
+const booksInProgressText = document.querySelector('.unfinished')
+
 
 
 function Book (title, author, pages , read) {
@@ -23,7 +23,6 @@ function Book (title, author, pages , read) {
     }
 }
 
-
 function addBookToLibrary (book) {
     return myLibrary.push(book)
 }
@@ -34,7 +33,27 @@ function showLibrary(lib) {
     }
 }
 
-// adevnent listener to btn and show up a modal with input for book author etc (fade background and show up the input modal)
+// ========= every new book created create new modal and pass author title etc and show it in dom
+function appendBook (book) {
+    const divTitle = document.createElement('div')
+    const divAuthor = document.createElement('div')
+    const divPages = document.createElement('div')
+    const divRead = document.createElement('div')
+    const bookContainer = document.querySelector('.bookcontainer')
+    const newModal = document.createElement('div')
+    newModal.classList.add('modal')
+    bookContainer.appendChild(newModal)
+    divTitle.textContent = ` Title: ${book.title} `
+    divAuthor.textContent = `Author: ${book.author} `
+    divPages.textContent = `Pages: ${book.pages}`
+    divRead.textContent = `Read: ${book.read}`
+    newModal.appendChild(divTitle)
+    newModal.appendChild(divAuthor)
+    newModal.appendChild(divPages)
+    newModal.appendChild(divRead)
+
+}
+//=========== adevnent listener to btn and show up a modal with input for book author etc (fade background and show up the input modal)
 btn.addEventListener('click', () => {
     if  (input.style.display === 'flex' ){
         input.style.display = 'none'
@@ -44,25 +63,30 @@ btn.addEventListener('click', () => {
 
 })
 
-// pass inputs as parameters in new Book
+//============ pass inputs as parameters in new Book
+submitbtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    const bookTitle = document.getElementById('title').value
+    const authorName = document.getElementById('author').value
+    const pagesRead = document.getElementById('pages').value
+    const read = document.querySelector('input[name=read]:checked').value;
+    document.querySelector('form').reset()
+    newBook = new Book(bookTitle, authorName, pagesRead , read)
+    addBookToLibrary(newBook)
+    showLibrary(myLibrary)
+    appendBook(newBook)
+    if (read === 'Yes'){
+        totalBooks++
+        totalBooksText.textContent = 'Books Finished: '
+        totalBooksText.textContent = totalBooksText.textContent + `${totalBooks}`
+    }
+    if (read === 'No'){
+        booksInProgress++
+        booksInProgressText.textContent = 'Books in Progress: '
+        booksInProgressText.textContent = booksInProgressText.textContent + `${booksInProgress}`
+    }
+    
+})
 
-
-// every new book created create new modal and pass author title etc and show it in dom
-
-const book1 = new Book('Hobbit', 'Tolkin', 150 , false)
-const book2 = new Book('Lord of the rings', 'Tolkin', 150, true)
-
-addBookToLibrary(book1)
-addBookToLibrary(book2)
-
-
-
-
-divTitle.textContent = ` Title: ${book1.title} `
-divAuthor.textContent = `Author: ${book1.author} `
-divPages.textContent = `Pages: ${book1.pages}`
-
-
-showLibrary(myLibrary)
 
 
