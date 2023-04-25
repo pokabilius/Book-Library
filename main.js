b19456c826107c50275f1cc5276d55aedeeaf233
pokabilius <georgemirsilidis@hotmail.com>
2023-04-25 23:31:23 +0300

    Should fix the way data is shown

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ================Variables=============================================
 
 let myLibrary = [];
 let newBook = {}
@@ -10,6 +11,10 @@ const btn = document.querySelector('button')
 const input = document.querySelector('.formcontainer')
 const totalBooksText = document.querySelector('.finished')
 const booksInProgressText = document.querySelector('.unfinished')
+const closebtn = document.querySelector('.closebtn')
+const header = document.querySelector('.header')
+const displaypanel = document.querySelector('.displaypanel')
+let closemodal = document.querySelector('.closemodal')
 
 
 
@@ -19,7 +24,7 @@ function Book (title, author, pages , read) {
     this.pages = pages
     this.read = read
     this.info = () => {
-        return `${title} + ${author} + ${pages}`
+        return `${title} + ${author}`
     }
 }
 
@@ -33,35 +38,99 @@ function showLibrary(lib) {
     }
 }
 
-// ========= every new book created create new modal and pass author title etc and show it in dom
+// ==================To do ===========================================================================================
+// =========for every new book created create new modal and pass author title etc and show it in dom
 function appendBook (book) {
-    const divTitle = document.createElement('div')
-    const divAuthor = document.createElement('div')
-    const divPages = document.createElement('div')
-    const divRead = document.createElement('div')
+    const divTitle = document.querySelector('.divTitle')
+    const divAuthor = document.querySelector('.divAuthor')
+    const divPages = document.querySelector('.divPages')
+    const divRead = document.querySelector('.divRead')
     const bookContainer = document.querySelector('.bookcontainer')
-    const newModal = document.createElement('div')
-    newModal.classList.add('modal')
-    bookContainer.appendChild(newModal)
-    divTitle.textContent = ` Title: ${book.title} `
-    divAuthor.textContent = `Author: ${book.author} `
-    divPages.textContent = `Pages: ${book.pages}`
-    divRead.textContent = `Read: ${book.read}`
-    newModal.appendChild(divTitle)
-    newModal.appendChild(divAuthor)
-    newModal.appendChild(divPages)
-    newModal.appendChild(divRead)
-
+    const modal = document.querySelector('.modal')
+    
+    if (myLibrary.length === 1) {
+        console.log("array of one book")
+        modal.style.display = 'flex'
+        divTitle.textContent = ` Title: ${book.title} `
+        divAuthor.textContent = `Author: ${book.author} `
+        divPages.textContent = `Pages: ${book.pages}`
+        divRead.textContent = `Read: ${book.read}`
+    }else{
+        const newdivTitle = document.createElement('div')
+        const newdivAuthor = document.createElement('div')
+        const newdivPages = document.createElement('div')
+        const newdivRead = document.createElement('div')
+        const newModal = document.createElement('div')
+        const titlecontainer = document.createElement('div')
+        closemodal = `<div class="material-symbols-outlined closemodal">close</div>`
+        console.log("aray bigger than one")
+        newdivTitle.textContent = ` Title: ${book.title} ` //separate data from titles in order to identify them later
+        newdivAuthor.textContent = `Author: ${book.author} ` //separate data from titles in order to identify them later
+        newdivPages.textContent = `Pages: ${book.pages}` //separate data from titles in order to identify them later
+        newdivRead.textContent = `Read: ${book.read}` //separate data from titles in order to identify them later
+        newModal.classList.add('modal')
+        newModal.style.display = 'flex'
+        titlecontainer.classList.add('titlecontainer')
+        bookContainer.appendChild(newModal)
+        titlecontainer.appendChild(newdivTitle)
+        titlecontainer.innerHTML += closemodal
+        newModal.appendChild(titlecontainer)
+        newModal.appendChild(newdivAuthor)
+        newModal.appendChild(newdivPages)
+        newModal.appendChild(newdivRead)
+    }
+    
 }
+// ==================To do ===========================================================================================
+
+
 //=========== adevnent listener to btn and show up a modal with input for book author etc (fade background and show up the input modal)
 btn.addEventListener('click', () => {
     if  (input.style.display === 'flex' ){
         input.style.display = 'none'
+        header.classList.remove('blur')
+        displaypanel.classList.remove('blur')
+        
     }else {
         input.style.display = 'flex'
+        header.classList.add('blur')
+        displaypanel.classList.add('blur')
     }
 
 })
+
+closebtn.addEventListener('click', () => {
+    if  (input.style.display === 'flex' ){
+        input.style.display = 'none'
+        header.classList.remove('blur')
+        displaypanel.classList.remove('blur')
+    }else {
+        input.style.display = 'flex'
+        header.classList.add('blur')
+        displaypanel.classList.add('blur')
+    }
+
+})
+
+closemodal.addEventListener('click',deleteBook)
+
+// ==================To do ===========================================================================================
+function deleteBook (e) {
+    //identify book with id(id=title )
+    let id = e.target.previousElementSibling.textContent
+    alert(id)
+    // delete book from array
+    for (i in myLibrary){
+        if (myLibrary[i].title === id){
+            myLibrary.splice(i,1)
+        }
+    }
+    // delete book from display panel 
+    
+    // update counters 
+}
+// ==================To do ===========================================================================================
+
 
 //============ pass inputs as parameters in new Book
 submitbtn.addEventListener('click', (event) => {
@@ -71,10 +140,11 @@ submitbtn.addEventListener('click', (event) => {
     const pagesRead = document.getElementById('pages').value
     const read = document.querySelector('input[name=read]:checked').value;
     document.querySelector('form').reset()
-    newBook = new Book(bookTitle, authorName, pagesRead , read)
+    newBook = new Book (bookTitle, authorName, pagesRead , read)
     addBookToLibrary(newBook)
     showLibrary(myLibrary)
     appendBook(newBook)
+    console.log(myLibrary)
     if (read === 'Yes'){
         totalBooks++
         totalBooksText.textContent = 'Books Finished: '
